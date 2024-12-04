0608bbc18ae2ea496e056a5b50d9596e0247c943
Daylion Templeton <templetondaylion@icloud.com>
2024-12-04 12:15:14 +0000

    readies code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pebi8x6$49jrr83uy3n0#f!$cc%wmxsn4bl(#-mn6@lr-466qk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-dayliontemp-ithinkthere-u06pfvfdvjh.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-dayliontemp-ithinkthere-u06pfvfdvjh.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
